981ac931a21e8796e9da691fc655662103632d08
satco <bubanja.kaju@gmail.com>
2018-11-28 23:02:27 +0100

    uncommented tensorboard summaries

--- lib/fcn/train.py
+++ lib/fcn/train.py
@@ -206,9 +206,9 @@ class SolverWrapper(object):
     def train_model_vertex_pose(self, sess, train_op, loss, loss_cls, loss_vertex, loss_pose, learning_rate, max_iters, data_layer):
         """Network training loop."""
         # add summary
-        # tf.summary.scalar('loss', loss)
-        # merged = tf.summary.merge_all()
-        # train_writer = tf.summary.FileWriter(self.output_dir, sess.graph)
+        tf.summary.scalar('loss', loss)
+        merged = tf.summary.merge_all()
+        train_writer = tf.summary.FileWriter(self.output_dir, sess.graph)
 
         coord = tf.train.Coordinator()
         if cfg.TRAIN.VISUALIZE:
@@ -231,7 +231,7 @@ class SolverWrapper(object):
 
         tf.get_default_graph().finalize()
 
-        # tf.train.write_graph(sess.graph_def, self.output_dir, 'model.pbtxt')
+        tf.train.write_graph(sess.graph_def, self.output_dir, 'model.pbtxt')
 
         last_snapshot_iter = -1
         timer = Timer()
@@ -239,7 +239,7 @@ class SolverWrapper(object):
 
             timer.tic()
             loss_value, loss_cls_value, loss_vertex_value, loss_pose_value, lr, _ = sess.run([loss, loss_cls, loss_vertex, loss_pose, learning_rate, train_op])
-            # train_writer.add_summary(summary, iter)
+            train_writer.add_summary(summary, iter)
             timer.toc()
             
             print 'iter: %d / %d, loss: %.4f, loss_cls: %.4f, loss_vertex: %.4f, loss_pose: %.4f, lr: %.8f,  time: %.2f' %\
