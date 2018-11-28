2734d01345c46a89e8e571ded2d91d6082d9a3de
satco <bubanja.kaju@gmail.com>
2018-11-28 16:07:27 +0100

    added visualization script for datasets

---
+++ generate_dataset/visualize_dataset.py
@@ -0,0 +1,24 @@
+import cv2
+import os
+from PIL import Image
+
+
+def main():
+    with open("config.yaml", "r") as config:
+        config_dict = yaml.load(config)
+    base_path = config_dict["datasets_base_path"]
+    lov_path = config_dict["lov_path"]
+    trainlist = open(os.path.join(lov_path, "trainval.txt"), "r")
+    for line in trainlist:
+        image_path = os.path.join(base_path, line + "-color.png")
+        mask_path = os.path.join(base_path, line + "-label.png")
+        image = cv2.imread(image_path)
+        mask = cv2.imread(mask_path)
+        alpha = 0.5
+        image_with_mask = cv2.addWeighted(mask, alpha, image, 1 - alpha, 0)
+        result = Image.fromarray(image_with_mask)
+        result.show()
+
+
+if __name__ == "__main__":
+    main()
