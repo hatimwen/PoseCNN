cb514062e5601a60aa493b2a9e8a9020b52d015a
Kaju Bubanja <bubanja.kaju@gmail.com>
2018-12-17 17:05:58 +0100

    updated gitignore

--- lib/fcn/train.py
+++ lib/fcn/train.py
@@ -262,7 +262,7 @@ class SolverWrapper(object):
 
         last_snapshot_iter = -1
         timer = Timer()
-        epochs = 45
+        epochs = 10000
         for epoch in range(epochs):
             coord_train.run = True
             coord_val.run = True
