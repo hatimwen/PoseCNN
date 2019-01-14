fe7d139d066af30069e98f71f5ec40e33c7c813f
Kaju Bubanja <bubanja.kaju@gmail.com>
2019-01-14 11:19:17 +0100

    fixed cache overwrite bug

--- lib/datasets/lov_single.py
+++ lib/datasets/lov_single.py
@@ -258,7 +258,7 @@ class lov_single(datasets.imdb):
         This function loads/saves from/to a cache file to speed up future calls.
         """
 
-        cache_file = os.path.join(self.cache_path, self.name + '_gt_roidb.pkl')
+        cache_file = os.path.join(self.cache_path, self.name + '_gt_roidb_val.pkl')
         if os.path.exists(cache_file):
             with open(cache_file, 'rb') as fid:
                 roidb = cPickle.load(fid)
