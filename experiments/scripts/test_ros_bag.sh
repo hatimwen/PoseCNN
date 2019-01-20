2cec3ad3222204888ec9c91f214a6a5a21e66c90
satco <bubanja.kaju@gmail.com>
2019-01-20 22:46:59 +0100

    updated visualize_dataset

--- experiments/scripts/test_ros_bag.sh
+++ experiments/scripts/test_ros_bag.sh
@@ -7,12 +7,13 @@ export PYTHONUNBUFFERED="True"
 export CUDA_VISIBLE_DEVICES=$1
 
 time ./ros/test_ros_bag.py --gpu 0 \
-  --bag bag/dataset1.3.bag \
+  --bag bag/dataset4.1.bag \
   --network vgg16_convs \
-  --model trained_nets/6/vgg16_fcn_color_single_frame_2d_pose_add_sym_lov_box_iter_62_epoch_19.ckpt \
+  --model trained_nets/run17/vgg16_fcn_color_single_frame_2d_pose_add_sym_lov_box_iter_62_epoch_10.ckpt \
   --imdb lov_single_000_box_train \
   --cfg experiments/cfgs/lov_color_box.yml \
   --rig data/LOV/camera.json \
   --cad data/LOV/models.txt \
   --pose data/LOV/poses.txt \
-  --background data/cache/backgrounds.pkl
+  --background data/cache/backgrounds.pkl \
+  --start_time 1541079570.808033
--- generate_dataset/visualize_dataset.py
+++ generate_dataset/visualize_dataset.py
@@ -7,14 +7,20 @@ import yaml
 def main():
     with open("config.yaml", "r") as config:
         config_dict = yaml.load(config)
-    base_path = config_dict["datasets_base_path"]
+    base_path = config_dict["images_path"]
     lov_path = config_dict["lov_path"]
     print(lov_path)
-    trainlist = open(os.path.join(lov_path, "000_box_train.txt"), "r")
-    for i, line in enumerate(trainlist):
-        line = line.rstrip()
-        image_path = os.path.join(base_path, line + "-color.png")
-        mask_path = os.path.join(base_path, line + "-label.png")
+    # trainlist = open(os.path.join(lov_path, "000_box_train.txt"), "r")
+    dataset = "dataset4.1"
+    trainlist = sorted(os.listdir(os.path.join(base_path, dataset)))
+    current = 0
+    for line in trainlist:
+        counter_str = line.split("-")[0]
+        if current == int(counter_str):
+            continue
+        current = int(counter_str)
+        image_path = os.path.join(base_path, dataset, counter_str + "-color.png")
+        mask_path = os.path.join(base_path, dataset, counter_str + "-label.png")
         image = cv2.imread(image_path, 1)
         mask = cv2.imread(mask_path, 1)
         alpha = 0.5
@@ -22,9 +28,7 @@ def main():
         # result = Image.fromarray(image_with_mask, "RGB")
         # result.show()
         cv2.imshow("image", image_with_mask)
-        cv2.waitKey(100)
-        # if i == 6:
-        #     break
+        cv2.waitKey(10)
 
 
 if __name__ == "__main__":
