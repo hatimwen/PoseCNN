b4aa457d7aee56058800bd83d06f33b55d50a6a5
satco <bubanja.kaju@gmail.com>
2019-02-06 17:54:37 +0100

    Pass session, so I don't create new one every batch

--- lib/gt_synthesize_layer/minibatch.py
+++ lib/gt_synthesize_layer/minibatch.py
@@ -24,14 +24,14 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 
 def get_minibatch(roidb, extents, points, symmetry, num_classes, backgrounds, intrinsic_matrix, \
-    data_queue, db_inds_syn, is_syn, db_inds_adapt, is_adapt, is_symmetric, class_colors=None):
+    data_queue, db_inds_syn, is_syn, db_inds_adapt, is_adapt, is_symmetric, class_colors=None, sess=None):
     """Given a roidb, construct a minibatch sampled from it."""
 
     # Get the input image blob, formatted for tensorflow
     random_scale_ind = npr.randint(0, high=len(cfg.TRAIN.SCALES_BASE))
     im_blob, im_depth_blob, im_normal_blob, im_scales, data_out, height, width = _get_image_blob(roidb, random_scale_ind, num_classes, backgrounds,
                                                                                                  intrinsic_matrix, data_queue, db_inds_syn, is_syn,
-                                                                                                 db_inds_adapt, is_adapt)
+                                                                                                 db_inds_adapt, is_adapt, sess)
 
     # build the label blob
     depth_blob, label_blob, meta_data_blob, vertex_target_blob, vertex_weight_blob, pose_blob, gt_boxes \
@@ -83,7 +83,7 @@ def get_minibatch(roidb, extents, points, symmetry, num_classes, backgrounds, in
 
     return blobs
 
-def _get_image_blob(roidb, scale_ind, num_classes, backgrounds, intrinsic_matrix, data_queue, db_inds_syn, is_syn, db_inds_adapt, is_adapt):
+def _get_image_blob(roidb, scale_ind, num_classes, backgrounds, intrinsic_matrix, data_queue, db_inds_syn, is_syn, db_inds_adapt, is_adapt, sess):
     """Builds an input blob from the images in the roidb at the specified
     scales.
     """
@@ -176,16 +176,16 @@ def _get_image_blob(roidb, scale_ind, num_classes, backgrounds, intrinsic_matrix
             im = chromatic_transform(im)
 
         random_hue = True
-        random_brightness = True
-        random_contrast = True
+        random_brightness = False
+        random_contrast = False
 
         if random_hue:
-            im = tf.image.random_hue(im)
+            with sess.as_default():
+                im = tf.image.random_hue(im, 0.5).eval()
         if random_brightness:
             im = tf.image.random_brightness(im)
         if random_contrast:
-            im = tf.image.random_brightness(im)
-
+            im = tf.image.random_contrast(im)
 
         if cfg.TRAIN.ADD_NOISE:
             im = add_noise(im)
